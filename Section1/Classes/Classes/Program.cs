993a76b1a2f309e89b1f6c866d94e28eebdcb384
Liana Simonian <liana.simoniian@gmail.com>
2024-09-18 11:06:44 +0300

    classes

---
+++ Section1/Classes/Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "Jhon";
+            person.Introduce("Mash");
+            var person1 = Person.Parse("Alen");
+            person1.Introduce("Mash");
+
+        }
+    }
+}
