fdf92e046e6139c608dceb6e4ee611cf8c7139ef
Liana Simonian <liana.simoniian@gmail.com>
2024-09-21 16:36:15 +0300

    object initialaizers

---
+++ Section1/ObjectInitializers/ObjectInitializers/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ObjectInitializers
+{
+    public class Person
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public DateTime Birthdate;
+
+        public Person()
+        {
+            
+        }
+        public Person(int id)
+        {
+            Id = id;   
+        }
+        public Person(int id, string firstName)
+        {
+            Id=id;
+            FirstName = firstName;
+        }
+        public Person(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public Person(int id, DateTime birthdate)
+        {
+            Id = id;
+            Birthdate = birthdate;
+        }
+    }
+}
