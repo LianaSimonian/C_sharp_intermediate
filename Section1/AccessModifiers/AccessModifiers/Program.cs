2c9b6710bee83cebb2d632819d4c6e4a05573fbe
Liana Simonian <liana.simoniian@gmail.com>
2024-09-21 17:13:11 +0300

    accessModifiers

---
+++ Section1/AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(DateTime.Now);
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
