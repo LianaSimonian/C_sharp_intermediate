8e266dd82db22bebdce8c4de1b2d3e00bdb74948
Liana Simonian <liana.simoniian@gmail.com>
2024-09-22 17:38:36 +0300

    exercise 1

---
+++ Section1/Exercises/Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.start();
+            Thread.Sleep(2000);
+            stopwatch.stop();
+            Console.WriteLine("Duration : "+ stopwatch.getDuration());
+
+            try
+            {
+                stopwatch.start();
+                stopwatch.start();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                stopwatch.stop();
+                stopwatch.stop();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            stopwatch.start();
+            Console.WriteLine("Duration: " + stopwatch.getDuration());
+            stopwatch.stop(); 
+        }
+    }
+}
