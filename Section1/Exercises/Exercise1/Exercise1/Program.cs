881d9b3a4119d08ebb0893c2429fcea1cc244c64
Liana Simonian <liana.simoniian@gmail.com>
2024-09-22 18:32:31 +0300

    exercise 1

---
+++ Section1/Exercises/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Stopwatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.start();
+            Thread.Sleep(2000);
+            stopwatch.stop();
+            Console.WriteLine("Duration :"+ stopwatch.GetDuration());
+
+            try
+            { 
+                stopwatch.start();
+                stopwatch .start();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                stopwatch.stop();
+                stopwatch.stop ();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
+                stopwatch.start();
+                Console.WriteLine(stopwatch.GetDuration());
+                stopwatch.stop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           
+        }
+    }
+}
---
+++ Section1/Exercises/Exercise1/Exercise1/Stopwatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    internal class Stopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private bool _isRunning;
+
+        public void start()
+        {
+            if (_isRunning)
+                throw new InvalidOperationException("Stopwatch is already running.please stop before starting a new one.");
+
+            _startTime = DateTime.Now;
+            _isRunning = true;
+            Console.WriteLine("Stopwatch started.");
+        }
+        public void stop()
+        {
+            if (!_isRunning)
+                throw new InvalidOperationException("Stopwatch has not been started. Please start the stopwatch before stopping it");
+
+            _stopTime = DateTime.Now;
+            _isRunning = false;
+            Console.WriteLine("Stopwatch stopped.");
+        }
+        public TimeSpan GetDuration()
+        {
+            if (_isRunning)
+                throw new InvalidOperationException("Stopwatch is still running. Please stop it before counting the  duration.");
+
+            return _stopTime - _startTime;
+        }
+    }
+}
