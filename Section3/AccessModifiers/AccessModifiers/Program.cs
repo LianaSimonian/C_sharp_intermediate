75f4f9bd661f295a1111215857654cad68f4e8c2
Liana Simonian <liana.simoniian@gmail.com>
2024-09-26 14:10:42 +0300

    accessModifiers

---
+++ Section3/AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();  
+
+        }
+    }
+}
