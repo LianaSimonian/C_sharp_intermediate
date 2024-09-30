1c9ff6ce28c0b564d02a804d2b703c02cff38a4c
Liana Simonian <liana.simoniian@gmail.com>
2024-09-30 17:42:07 +0300

    abstract classes

---
+++ Section4/AbstractClasses/AbstractClasses/Circle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace AbstractClasses
+{
+    internal class Circle:Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle .");
+        }
+    }
+}
