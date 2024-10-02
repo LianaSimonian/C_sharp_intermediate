fb0e553f6e9e41f80bc0bd521f45e57b85e1b4e2
Liana Simonian <liana.simoniian@gmail.com>
2024-10-02 18:46:12 +0300

    Testability

---
+++ Section5/Testability/Testability/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Testability
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var orderProcessor = new OrderProcessor(new ShippingCalculator());
+            var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 10f };
+            orderProcessor.Process(order);
+            Console.WriteLine("order shipment cost {0}, order shipment shippingDate {1}",order.Shipment.Cost,order.Shipment.ShippingDate);
+        }
+    }
+}
